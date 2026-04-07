b27370c04c60bde57ddf84f719439749aa192bd6
phbm2003 <145854692+phbm2003@users.noreply.github.com>
2026-04-06 22:54:19 -0300

    Update vigenere.cpp

--- vigenere.cpp
+++ vigenere.cpp
@@ -7,20 +7,32 @@
 #include <stdint.h>
 #include <cctype>
 
-double portuguese_freq[26] = {
-    14.63, 1.04, 3.88, 4.99, 12.57, 1.02, 1.30,
-    0.78, 6.18, 0.40, 0.02, 2.78, 4.74,
-    5.05, 10.73, 2.52, 1.20, 6.53,
-    7.81, 4.34, 4.63, 1.67, 0.01, 0.21,
-    0.01, 0.47
+double portuguese_freq[95] = {
+    16, 0.00005, 0.15, 0.0000005,  0.00005, 0.0000005, 0.0005, 0.15, 0.033, 0.033,
+    0.00004, 0.00015, 0.86, 0.2, 1.1, 0.05, 0.6, 0.5, 0.27, 0.21,
+    0.18, 0.25 , 0.19, 0.18, 0.25, 0.24, 0.01, 0.0007, 0.04, 0.00002,
+    0.04, 0.0004, 0.000006, 0.24, 0.17, 0.3, 0.1, 0.1, 0.1, 0.1,
+    0.08, 0.2, 0.08, 0.03, 0.1, 0.1, 0.1, 0.08, 0.02, 0.01, 
+    0.1, 0.3, 0.3, 0.1, 0.02, 0.08, 0.006, 0.025, 0.0086, 0.00009,
+    0.0000005, 0.00006, 0.00022, 0.000012, 0.0000005, 14.63, 1.04, 3.88, 4.99, 12.57, 
+    1.02, 1.30, 0.78, 6.18, 0.40, 0.02, 2.78, 4.74, 5.05, 10.73, 
+    2.52, 1.20, 6.53, 7.81, 4.34, 4.63, 1.67, 0.01, 0.21, 0.01,
+    0.47, 0.000006, 0.00005, 0.00005, 0.00002
 };
     
-double english_freq[26] = {
+double english_freq[95] = {      
+    20,  0.00005, 0.15, 0.0000005,  0.00005, 0.0000005, 0.0005, 0.15, 0.033, 0.033,
+    0.00004, 0.00015, 0.86, 0.2, 1.1, 0.05, 0.6, 0.5, 0.27, 0.21,
+    0.18, 0.25 , 0.19, 0.18, 0.25, 0.24, 0.01, 0.0007, 0.04, 0.00002,
+    0.04, 0.0004, 0.000006, 0.24, 0.17, 0.3, 0.1, 0.1, 0.1, 0.1,
+    0.08, 0.2, 0.08, 0.03, 0.1, 0.1, 0.1, 0.08, 0.02, 0.01, 
+    0.1, 0.3, 0.3, 0.1, 0.02, 0.08, 0.006, 0.025, 0.0086, 0.00009,
+    0.0000005, 0.00006, 0.00022, 0.000012, 0.0000005,
     8.17, 1.49, 2.78, 4.25, 12.70, 2.23, 2.02,
     6.09, 6.97, 0.15, 0.77, 4.03, 2.41,
     6.75, 7.51, 1.93, 0.10, 5.99,
     6.33, 9.06, 2.76, 0.98, 2.36, 0.15,
-    1.97, 0.07
+    1.97, 0.07, 0.000006, 0.00005, 0.00005, 0.00002
 };
 
 std::string build_output_file_path(const std::string &input_file_path, const std::string &suffix)
@@ -38,13 +50,9 @@ void convert_key_to_shift_array(std::string key, std::vector<uint8_t> &shift_vec
 {
     for (auto c : key)
     {
-        if (c >= 'A' && c <= 'Z')
+        if (c >= 0x00 && c <= 0x7F)
         {
-            shift_vector.push_back(c - 'A');
-        }
-        else if (c >= 'a' && c <= 'z')
-        {
-            shift_vector.push_back(c - 'a');
+            shift_vector.push_back(c - ' ');
         }
         else
         {
@@ -57,18 +65,15 @@ void convert_plain_text_to_int_array(std::string plain_text, std::vector<uint8_t
 {
     for (auto c : plain_text)
     {
-        if (c >= 'A' && c <= 'Z')
+        if (c >= ' ' && c <= '~')
         {
-            plain_text_vector.push_back(c - 'A');
+            plain_text_vector.push_back(c - ' ');
         }
-        else if (c >= 'a' && c <= 'z')
-        {
-            plain_text_vector.push_back(c - 'a');
-        }
-        else if (c == ' ')
+        
+        /*else if (c == ' ')
         {
             plain_text_vector.push_back(c);
-        }
+        }*/
         else
         {
             continue;
@@ -86,15 +91,15 @@ void vigenere(std::string plain_text, std::string key, std::string &cipher_text)
 
     for (auto c : plain_text_vector)
     {
-        if (c == ' ')
+        /*if (c == ' ')
         {
             cipher_text.push_back(c);
             continue;
-        }
+        }*/
 
         c += shift_vector.at(key_position);
 
-        cipher_text.push_back(((c % 26) + 'A'));
+        cipher_text.push_back(((c % 95) + ' '));
         key_position++;
         if (key_position >= shift_vector.size())
         {
@@ -113,14 +118,14 @@ void reverse_vigenere(std::string cipher_text, std::string key, std::string &pla
 
     for (auto c : cipher_text_vector)
     {
-        if (c == ' ')
+        /*if (c == ' ')
         {
             plain_text.push_back(c);
             continue;
-        }
+        }*/
 
         uint8_t shift = shift_vector.at(key_position);
-        plain_text.push_back(((c + 26 - shift) % 26) + 'A');
+        plain_text.push_back((c + 95 - shift) % 95 + ' ');
         key_position++;
         if (key_position >= shift_vector.size())
         {
@@ -160,33 +165,33 @@ std::vector<int> frequency(const std::string &group)
     std::vector<int> freq(26, 0);
 
     for (char c : group)
-        freq[c - 'A']++;
+        freq[c - 'a']++;
 
     return freq;
 }
 
 // Descobre letra da chave
-char find_key_letter(const std::string &group, double freq_table[26])
+char find_key_letter(const std::string &group, double freq_table[95])
 {
     int n = group.size();
     double best_score = 1e9;
     int best_shift = 0;
 
     // testa todos os shifts possíveis
-    for (int shift = 0; shift < 26; shift++)
+    for (int shift = 0; shift < 95; shift++)
     {
-        std::vector<int> freq(26, 0);
+        std::vector<int> freq(95, 0);
 
         // aplica "descriptografia" com o shift
         for (char c : group)
         {
-            int decrypted = (c - 'A' - shift + 26) % 26;
+            int decrypted = (c - ' ' - shift + 95) % 95;
             freq[decrypted]++;
         }
 
         // calcula erro (chi-square simplificado)
         double score = 0.0;
-        for (int i = 0; i < 26; i++)
+        for (int i = 0; i < 95; i++)
         {
             double expected = freq_table[i] * n / 100.0;
             if (expected > 0)
@@ -204,11 +209,11 @@ char find_key_letter(const std::string &group, double freq_table[26])
         }
     }
 
-    return 'A' + best_shift;
+    return best_shift + ' ';
 }
 
 // Descobre chave inteira
-std::string find_key(const std::string &text, int key_size, double freq_table[26])
+std::string find_key(const std::string &text, int key_size, double freq_table[95])
 {
     auto groups = split_groups(text, key_size);
     std::string key;
@@ -235,18 +240,18 @@ std::vector<int> find_key_size(const std::string &text, double constante)
 
         for (auto &g : groups) {
             if (g.size() < 2) continue;
-            std::vector<int> freq(26, 0);
+            std::vector<int> freq(95, 0);
 
             // computa frequências de cada letra
             for (char c : g) {
-                int index = (c - 'A' + 26) % 26;
+                int index = (c - ' ') % 95;
                 freq[index]++;
             }
 
             double ic = 0.0;
             int N = g.size();
 
-            for (int i = 0; i < 26; i++) {
+            for (int i = 0; i < 95; i++) {
                 ic += freq[i] * (freq[i] - 1);
             }
 
@@ -332,15 +337,15 @@ int main(int argc, char *argv[])
         }  
         else if (operation == "attack")
         {
-            std::string cleaned = clean_text(input_text);
+            //std::string cleaned = clean_text(input_text);
 
             std::cout << "=== ATTACK MODE ===" << std::endl;
 
-            std::vector<int> size_pt = find_key_size(cleaned, 0.07813849);
-            std::vector<int> size_en = find_key_size(cleaned, 0.06549669);
+            std::vector<int> size_pt = find_key_size(input_text, 0.07813849);
+            std::vector<int> size_en = find_key_size(input_text, 0.06549669);
 
             for (int size : size_pt) {
-                std::string key_pt = find_key(cleaned, size, portuguese_freq);
+                std::string key_pt = find_key(input_text, size, portuguese_freq);
                 std::string dec_pt;
                 reverse_vigenere(input_text, key_pt, dec_pt);
 
@@ -351,7 +356,7 @@ int main(int argc, char *argv[])
             std::cout << std::endl;
 
             for (int size : size_en) {
-                std::string key_en = find_key(cleaned, size, english_freq);
+                std::string key_en = find_key(input_text, size, english_freq);
                 std::string dec_en;
                 reverse_vigenere(input_text, key_en, dec_en);
 
